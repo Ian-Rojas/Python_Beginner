ff0439fb193868ff42204ab70075e7cd77108fa7
Ian-Rojas <joshue3345@gmail.com>
2026-04-23 15:32:04 -0400

    ejercicios con strings

---
+++ 23-04.py
@@ -0,0 +1,27 @@
+# 1.- Ejercicio
+
+# passw="SHAZAM"
+# user=input("Ingrese la contraseña: ")
+
+# if user.upper() == passw:
+#     print("Acceso Concedido")
+# else:
+#     print("Acceso Denegado")
+
+# user = input("Crea un nombre de usuario: ")
+
+# 2.- Ejercicio
+
+# if len(user) >= 4 and len(user) <= 10:
+#     print("Su nombre de usuario ha sido creado.")
+# else:
+#     print("Su nombre de usuario no esta en el rango preestablecido")
+
+# 3.- Ejercicio
+
+# num = int(input("Cree un pin: "))
+
+# if len(str(num))==4:
+#     print("Su contraseña pin ha sido creada.")
+# else:
+#     print("Su contraseña supera el rango establecido")
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Python_Beginner
+# 🐍 Mis Trabajos de Python
+
+Bienvenido a mi colección de trabajos y scripts de Python. Aquí guardo desde ejercicios de lógica hasta pequeñas aplicaciones funcionales.
+
+---
